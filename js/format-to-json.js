919f661ff1ece20dd4b35595e961e48274b12d8f
abhishekroy666 <roy.ironmaiden@gmail.com>
2019-04-30 14:30:09 +0530

    #2 : prototype for formatting json in <body> and <pre> tags only

--- js/format-to-json.js
+++ js/format-to-json.js
@@ -1,52 +1,141 @@
 
+// author : abhishek.roy
+//----------------------------------------CODE START------------------------------------------------//
 var body = document.getElementsByTagName("body")[0];
 
-var preformatted_data = body.getElementsByTagName("pre")[0];
+var json;
 
-try{
-	var table = display(JSON.parse(preformatted_data.innerHTML));
-	//table = display(data);
-	$(preformatted_data).hide();
-	$(body).append(table);
-}catch(err){
-	alert("Invalid JSON Format");
+try {
+    json = JSON.parse(body.innerHTML);
+} catch (error) {
+    try {
+        var preJson = body.getElementsByTagName("pre")[0];
+        json = JSON.parse(preJson);
+        alert("JSON is preformatted");
+    } catch (error) {
+        alert(error);
+    }
 }
 
-function display(obj) {
-	if(typeof obj === "object"){
-		if(obj.length === undefined){
-			var objTbl = '<div class="container">';
-			objTbl += '<div class="header expand row"><div class="row">&nbsp&nbsp[-]<span class="sign"></span></div></div>';
-			for(var key in obj){
-				objTbl += '<div class="row">';
-				objTbl += '<div class="col-*-*">'+key+'</div>&nbsp&nbsp : &nbsp&nbsp';
-				objTbl += '<div class="col-*-*">'+display(obj[key])+'</div>';
-				objTbl += '</div>';
+if( !isEmpty(json) ) {
+    var header =    '<div class="container-fluid">'+
+                    '<div class="row">'+
+                    '<div class="col-*-*" style="padding:15px;"><button id="expand-all" type="button" class="btn btn-sm btn-info">Expand All</button></div>'+
+                    '<div class="col-*-*" style="padding:15px;"><button id="collapse-all" type="button" class="btn btn-sm btn-info">Collapse All</button></div>'+
+                    '</div>'+
+                    '</div><b>{<b>';
+    var divs = header + buildHTML(json)+'<b>}</b>';
+    $(body).html(divs);
+    $(body).css({ 'font-size': '12px',
+                'font-family':'consolas' });
+}
+
+//----------------------------------------CODE END------------------------------------------------//
+
+//----------------------------------------FUNCTIONS------------------------------------------------//
+
+/**
+ * @param {*} object 
+ * @description : function to check if an object is empty
+ * @returns boolean
+ */
+function isEmpty ( object ) {
+    for(var key in object){
+        return false;
+    }
+    return true;
+}
+
+/**
+ * @param {*} jsonData 
+ * @description : function to build <div> grid from jsonData
+ * @returns String
+ */
+function buildHTML(jsonData) {
+    var div;
+    if(typeof jsonData === "object"){
+		if(jsonData.length === undefined){
+			div = '<div class="container-fluid">'; 
+            div += '<div class="header">'+String.fromCharCode(0x25BD)+'</div><b>{</b>&ensp;';
+            div += '<div class="content">';
+			for(var key in jsonData){   
+                div += '<div class="row">'; 
+                div += '<div class="col-*-*">'+"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"+String.fromCharCode(0x25FE)+"&ensp;"+'</div>'; 
+				div += '<div class="col-*-*">'+"&ensp;<b>"+key+"</b>&ensp; :"+'</div>'; 
+				div += '<div class="col-10">'+buildHTML(jsonData[key])+'</div>'; 
+				div += '</div>';    
 			}	
-			objTbl += '</div>';
-			return objTbl;
+			div += '</div><b>}</b>,</div>';    
+			return div;
 		} else {
-			var arrTbl = '<div class="container">';
-			var i = 0;
-			for(item in obj){
-				arrTbl += '<div class="row">';
-				arrTbl += '<div class="col-*-*">('+(i++)+')</div>&nbsp&nbsp:&nbsp&nbsp';
-				arrTbl += '<div class="col-*-*">'+display(obj[item])+'</div>';
-				arrTbl += '</div>';
+            var i = 0;
+			div = '<div class="container-fluid">';
+			div += '<div class="header">'+String.fromCharCode(0x25BD)+'</div><b>[</b>&ensp;'; 
+			div += '<div class="content">';
+			for(item in jsonData){
+				div += '<div class="row">';
+				div += '<div class="col-*-*">'+'&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i>['+(i++)+"]</i>&nbsp;:"+'</div>';
+				div += '<div class="col-10">'+buildHTML(jsonData[item])+'</div>';
+				div += '</div>';
 			}	
-			arrTbl += '</div>';
-			return arrTbl;
+			div += '</div><b>]</b>,</div>';
+			return div;
 		}
 	}else{
-		return obj;
+        if(typeof jsonData === "number") {
+            return '<b><font color="green">' + jsonData + '</font></b>,';
+        }
+        if(typeof jsonData === "string") {
+            return '<font color="crimson">"' + jsonData + '"</font>,';
+        }
+        if(typeof jsonData === "boolean") {
+            return '<b><font color="blue">' + jsonData + '</font></b>,';
+        }
+		return jsonData + ',';
 	}
 }
 
-$('.header').click(function(){
-	if(this.innerHTML === "[+]"){
-		this.innerHTML = "[-]";
-	}else{
-		this.innerHTML = "[+]";
-	}
-    $(this).toggleClass('expand').nextUntil('div.header').slideToggle(100);
+
+//----------------------------------------JQUERY------------------------------------------------//
+
+/**
+ * on clicking .header class objects :
+ * 1. the visibility of .content class objects are toggled.
+ * 2. the text inside .header class objects are changed.
+ */
+$(".header").click(function () {
+    $header = $(this);
+    $content = $header.next().next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? String.fromCharCode(0x25BD) : String.fromCharCode(0x25BA);
+        });
+    });
+
 });
+
+/**
+ * collapse all .content objects
+ */
+$("#collapse-all").click(function(){
+    $content = $(".header").next().next();
+    if($content.is(":visible")){
+        $content.hide();
+    }
+});
+
+/**
+ * expand all .content objects
+ */
+$("#expand-all").click(function(){
+    $content = $(".header").next().next();
+    if(! $content.is(":visible")){
+        $content.show();
+    }
+});
+
+//----------------------------------------XXXXXXXXXX------------------------------------------------//
--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,5 @@
-
-function clickJsonButton(e){
-	chrome.tabs.insertCSS(null, { file: "css/bootstrap.min.css" });
-	chrome.tabs.executeScript(null, {file: "js/jquery-3.4.0.js"});
-	chrome.tabs.executeScript(null, { file : "js/format-to-json.js" });
-	window.close();
-}
-
-function clickTextButton(e){
-	chrome.tabs.executeScript(null, { code : "window.location = window.location.href;" });
-	window.close();
-}
-
+// author : abhishek.roy
+//----------------------------------------CODE START------------------------------------------------//
 document.addEventListener('DOMContentLoaded', function () {	
 	var jsonButton = document.getElementById("json");
 	var textButton = document.getElementById("text");
@@ -19,3 +8,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	textButton.addEventListener('click', clickTextButton);
 });
 
+//----------------------------------------CODE END------------------------------------------------//
+
+//----------------------------------------FUNCTIONS------------------------------------------------//
+
+/**
+ *  * @param {*} e : event
+ *  executes when said event is fired
+ *  injects CSS + loads and executes JS ---> on current-tab DOM
+ */
+function clickJsonButton(event){
+	chrome.tabs.insertCSS(null, { file: "css/bootstrap.min.css" });
+	chrome.tabs.executeScript(null, {file: "js/jquery-3.4.0.js"});
+	chrome.tabs.executeScript(null, { file : "js/format-to-json.js" });
+	window.close();
+}
+
+/**
+ *  * @param {*} e : event
+ *  executes when said event is fired
+ *  refreshes the browser tab to load default text/JSON
+ */
+function clickTextButton(event){
+	chrome.tabs.executeScript(null, { code : "window.location = window.location.href;" });
+	window.close();
+}
+
+//----------------------------------------XXXXXXXXXX------------------------------------------------//
